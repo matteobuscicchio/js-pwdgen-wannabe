f2198264782fa0f98d7d6256f2d0c4c72450bd95
matteo <matteoezio@me.com>
2020-11-12 16:33:21 +0100

    struttura di base
    funziona

---
+++ assets/js/main.js
@@ -0,0 +1,7 @@
+var userName = prompt("Come ti chiami?");
+
+var userSurname = prompt("Qual è il tuo cognome?");
+
+var userColor = parseInt(prompt("Qual è il tuo colore preferito?"));
+
+document.getElementById('name', 'surname').innerHTML = userName + userSurname + "19";
