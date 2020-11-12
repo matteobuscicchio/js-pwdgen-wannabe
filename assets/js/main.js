faa9ea7e3f48703bfc5f1011c8ff3cc381673d16
matteo <matteoezio@me.com>
2020-11-12 17:38:03 +0100

    corretto il file js
    corretta riga 8 del file main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,8 @@
+
 var userName = prompt("Come ti chiami?");
 
 var userSurname = prompt("Qual è il tuo cognome?");
 
 var userColor = prompt("Qual è il tuo colore preferito?");
 
-document.getElementById('name', 'surname', 'color').innerHTML = userName + userSurname + userColor + "19";
+document.getElementById('name').innerHTML = userName + userSurname + userColor + "19";
