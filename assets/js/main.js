dbf75c6b397a333d5dbeccc06c748026eecb2ae8
matteo <matteoezio@me.com>
2020-11-12 16:34:42 +0100

    aggiunta domanda su colore preferito

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,6 +2,6 @@ var userName = prompt("Come ti chiami?");
 
 var userSurname = prompt("Qual è il tuo cognome?");
 
-var userColor = parseInt(prompt("Qual è il tuo colore preferito?"));
+var userColor = prompt("Qual è il tuo colore preferito?");
 
-document.getElementById('name', 'surname').innerHTML = userName + userSurname + "19";
+document.getElementById('name', 'surname', 'color').innerHTML = userName + userSurname + userColor + "19";
